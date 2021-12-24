3fd48b4b5f89c78737dd8c717855720a195d9ab0
young123 <18588880154@163.com>
2021-12-24 12:08:28 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ubuntu:20.04
+
+LABEL MAINTAINER ="Young"
+
+COPY docker-entrypoint.sh /usr/local/bin/
+
+RUN chmod +x /usr/local/bin/docker-entrypoint.sh && \
+    apt-get update && apt-get install -y mysql-client && \
+    rm -rf /var/lib/apt/lists/*
+
+ENTRYPOINT ["docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+DBNAME=${MYSQL_DATABASE}
+
+HOSTNAME=${MYSQL_HOST}
+
+PASSWORD=${MYSQL_ROOT_PASSWORD}
+
+PORT=${MYSQL_PORT}
+
+USERNAME=root
+
+LOGIN_CMD="mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}"
+
+LOCK_PATH=/root/created_database
+
+echo "${LOGIN_CMD}"
+
+create_database() {
+    echo "create database ${DBNAME}"
+    create_db_sql="create database if not exists ${DBNAME} character set utf8"
+    echo "${create_db_sql}" | ${LOGIN_CMD}
+
+    if [ $? -ne 0 ]
+    then
+        echo "create database ${DBNAME} failed..."
+        exit 1
+    else
+        echo "succeed to create database ${DBNAME}"
+    fi
+}
+
+Generate_lock_file(){
+    mount | grep ${LOCK_PATH} > /dev/null
+    if [[ $? != 0 ]];then
+        echo -e "\033[43;37m[ WARN ]\033[0m The lock path in which the lock file must be persisted! Check \$LOCK_PATH"
+    fi
+    echo -e "\033[42;37m[ INFO ]\033[0m Generating the lock file ${LOCK_PATH}/.datainited"
+    touch ${LOCK_PATH}/.datainited
+}
+
+if [[ ! -f ${LOCK_PATH}/.datainited ]];then
+    create_database && Generate_lock_file
+else
+    echo -e "\033[42;37m[ INFO ]\033[0m The data has already been initialized, skiping..."
+fi
+
